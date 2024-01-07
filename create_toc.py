6ad2d0b18e1033e0a0b2446bf6c8fd990e30cf00
kdog3682 <kdog3682@gmail.com>
2024-01-06 22:04:39 -0800

    auto push

---
+++ create_toc.py
@@ -0,0 +1,52 @@
+from utils import *
+
+def get_toc_json(root, ignore = default_path_ignore):
+    root_dir = Path(root)
+    def recurse(directory):
+
+        store = {}
+        store["name"] = str(directory)
+        store["type"] = "directory"
+        store["contents"] = []
+
+        for path in directory.iterdir():
+            if ignore(path):
+                continue
+
+            value = None
+            if path.is_dir():
+                value = recurse(path)
+            elif path.is_file():
+                value = {"type": "file", "name": path.name}
+
+            if value == None: continue
+            store["contents"].append(value)
+
+        if empty(store["contents"]):
+            return None
+        return store
+
+    return recurse(root_dir)
+
+
+def get_toc_string(data):
+    def runner(state):
+        name = state.get("name")
+        type = state.get("type")
+        contents = state.get("contents")
+
+        if type == "directory":
+            items = map(contents, runner)
+            s = "dir: " + name + "\n"
+            s += indent(join(items, "\n"))
+            return s
+        else:
+            return name
+    return runner(data)
+
+
+def create_toc(dir):
+    write(tocfile, get_toc_string(get_toc_json(dir)))
+
+# file = get_most_recent_file(typstpackagedir)
+# create_toc(file)
